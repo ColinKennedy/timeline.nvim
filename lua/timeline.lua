00f6fe7f5d9e0304558f2000ef6ce1a05876cfe9
Colin Kennedy <colinvfx@gmail.com>
2024-01-07 21:26:19 -0800

    Added timeline.nvim

---
+++ lua/timeline.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_hl(0, "TimelineNvimGitCommitAuthor", {default=true, link="String"})
+vim.api.nvim_set_hl(0, "TimelineNvimGitCommitCommit", {default=true, link="Special"})
+vim.api.nvim_set_hl(0, "TimelineNvimGitCommitDate", {default=true, link="Number"})
+vim.api.nvim_set_hl(0, "TimelineNvimGitCommitEmail", {default=true, link="Text"})
